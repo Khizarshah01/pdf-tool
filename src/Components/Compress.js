6d906dc9f73ba23bb457697399d108e45b2892ca
khizarshah01 <khizar.shah010@gmail.com>
2024-08-10 21:34:52 +0530

    compress backend connected properly

--- src/Components/Compress.js
+++ src/Components/Compress.js
@@ -6,23 +6,28 @@ function Compress() {
     <div className="bodys">
       <h2>Compress</h2>
       
-      <form className={Compres.formdiv} action="http://localhost:5000/compress" method="post" encType="multipart/form-data">
-        <input type='file' accept="application/pdf" ></input>
+      <form 
+        className={Compres.formdiv} 
+        action='http://localhost:5000/compress' 
+        method='post' 
+        encType="multipart/form-data"
+      >
+        <input type='file' accept="application/pdf" name="pdfFile"></input>
         <div className={Compres.radioGroup}>
           <div>
-          <input type="radio" id="low" name="compression" value="low" className={Compres.radioInput} />
-          <label htmlFor="low" className={Compres.label}>Low</label>
+            <input type="radio" id="low" name="compression" value="low" className={Compres.radioInput} />
+            <label htmlFor="low" className={Compres.label}>Low</label>
           </div>
         
-        <div>
-          <input type="radio" id="medium" name="compression" value="medium" className={Compres.radioInput} />
-          <label htmlFor="medium" className={Compres.label}>Medium</label>
-        </div>
+          <div>
+            <input type="radio" id="medium" name="compression" value="medium" className={Compres.radioInput} />
+            <label htmlFor="medium" className={Compres.label}>Medium</label>
+          </div>
         
-        <div>
-          <input type="radio" id="high" name="compression" value="high" className={Compres.radioInput} />
-          <label htmlFor="high" className={Compres.label}>High</label>
-        </div>
+          <div>
+            <input type="radio" id="high" name="compression" value="high" className={Compres.radioInput} />
+            <label htmlFor="high" className={Compres.label}>High</label>
+          </div>
         </div>
         <input className={Compres.submitButton} type='submit' value={"Submit Pdf"}></input>
       </form>
